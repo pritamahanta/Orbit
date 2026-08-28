c2cbf5a42bb5eb408939038d713b745c840cc51b
Pritam Mohanta <pm2721@it.jgec.ac.in>
2026-08-28 13:38:49 +0530

    feat: add error handling middleware and async handler utility

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({});
+
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -8,6 +9,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 
 const app = express();
@@ -31,6 +33,7 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+app.use(errorMiddleware);
 
 
 app.listen(PORT, () => {
